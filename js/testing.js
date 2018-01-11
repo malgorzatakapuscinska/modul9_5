0e5157a38a1f4fe27ad83b04d2a64c1e5d125303
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-01-11 12:11:10 +0100

    Test method showed in module 9_5

--- js/testing.js
+++ js/testing.js
@@ -7,6 +7,7 @@ function raiseAlarm() {
 }
 
 var delayedAlert = setTimeout(raiseAlarm,10000);
+clearTimeout(delayedAlert);
 
 //
 
@@ -27,3 +28,26 @@ console.log(navigation);
 var withMenuItemsClass = document.getElementsByClassName('menuItem');
 
 console.log(withMenuItemsClass);
+
+//wyszukanie elementów html o clasie menuItem znajdujących się w elemencie o id = menu - zwraca tablicę elementów
+var menu = document.getElementById('Menu');
+var withMenuItemsClassInNav = menu.getElementsByClassName('menuItem');
+console.log(withMenuItemsClassInNav);
+
+var itemsByTagname = document.getElementsByTagName('li');
+console.log(itemsByTagname);
+
+var navigation = document.getElementById('nav');
+console.log(navigation.nextElementSibling);
+
+//tworzenie nowego elementu p i dołaczenie go do html dokumentu na samym końcu elementu rodzica
+var navigation = document.getElementById('nav');
+var newElement = document.createElement('p');
+newElement.innerHTML = 'The text inside the p tag, which is under newElem';
+navigation.appendChild(newElement);
+
+//dodawanie nowej klasy do elementu
+
+var menu = document.getElementById('Menu');
+menu.className += "navbar-right";
+console.log(menu.className);
